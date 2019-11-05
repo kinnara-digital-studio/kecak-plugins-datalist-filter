cdb6eed41579b7366ba24f4311771dbec1754828
aristosh <aristo.hadisoeganda@gmail.com>
2019-11-05 22:54:58 +0700

    add Filter Value DateTime function

--- src/main/java/com/kinnara/kecakplugins/advancedatalistfilter/DateTimeDataListFilter.java
+++ src/main/java/com/kinnara/kecakplugins/advancedatalistfilter/DateTimeDataListFilter.java
@@ -5,6 +5,7 @@ import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.datalist.model.DataList;
 import org.joget.apps.datalist.model.DataListFilterQueryObject;
 import org.joget.apps.datalist.model.DataListFilterTypeDefault;
+import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.PluginManager;
 
 import java.text.DateFormat;
@@ -15,6 +16,7 @@ import java.util.Map;
 public class DateTimeDataListFilter extends DataListFilterTypeDefault {
     private final static DateFormat hibernateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
+    @Override
     @SuppressWarnings("unchecked")
     public String getTemplate(DataList datalist, String name, String label) {
         PluginManager pluginManager = (PluginManager) AppUtil.getApplicationContext().getBean("pluginManager");
@@ -33,6 +35,7 @@ public class DateTimeDataListFilter extends DataListFilterTypeDefault {
         return pluginManager.getPluginFreeMarkerTemplate(dataModel, getClassName(), "/templates/DatetimeDataListFilter.ftl", null);
     }
 
+    @Override
     public DataListFilterQueryObject getQueryObject(DataList datalist, String name) {
         DataListFilterQueryObject queryObject = new DataListFilterQueryObject();
 
@@ -51,17 +54,27 @@ public class DateTimeDataListFilter extends DataListFilterTypeDefault {
         valueFrom = valueFrom == null || valueFrom.isEmpty() ? "1970-01-01 00:00:00" : (showTime ? valueFrom : valueFrom + " 00:00:00");
         valueTo = valueTo == null || valueTo.isEmpty() ? "9999-12-31 23:59:59" : (showTime ? valueTo : valueTo + " 23:59:59");
 
-        if (datalist != null && datalist.getBinder() != null && valueFrom != null && !valueFrom.isEmpty() && valueTo != null && !valueTo.isEmpty()) {
+        LogUtil.info(getClassName(), "column ["+name+"] name ["+datalist.getBinder().getColumnName(name)+"]");
+        if (datalist != null && datalist.getBinder() != null && !valueFrom.isEmpty() && !valueTo.isEmpty()) {
             String databaseDateFunction = getPropertyString("databaseDateFunction");
+            String filterDateFunction = getPropertyString("filterDateFunction");
+
+            StringBuilder sb = new StringBuilder();
             if(databaseDateFunction == null || databaseDateFunction.isEmpty()) {
-                queryObject.setQuery(String.format("CAST(%s AS DATETIME) BETWEEN CAST(? AS DATETIME) AND CAST(? AS DATETIME)", datalist.getBinder().getColumnName(name)));
+                sb.append("CAST(%s AS DATETIME)");
             } else {
-                queryObject.setQuery(
-                        String.format("%s BETWEEN %s AND %s",
-                                databaseDateFunction.replaceAll("\\?", datalist.getBinder().getColumnName(name)),
-                                databaseDateFunction,
-                                databaseDateFunction));
+                sb.append(databaseDateFunction.replaceAll("\\?", datalist.getBinder().getColumnName(name)));
             }
+
+            sb.append(" BETWEEN ");
+
+            if(filterDateFunction == null || filterDateFunction.isEmpty()) {
+                sb.append("CAST(? AS DATETIME) AND CAST(? AS DATETIME)");
+            } else {
+                sb.append(String.format("%s AND %s", filterDateFunction, filterDateFunction));
+            }
+
+            queryObject.setQuery(sb.toString());
             queryObject.setValues(new String[]{valueFrom, valueTo});
 
             return queryObject;
@@ -69,27 +82,32 @@ public class DateTimeDataListFilter extends DataListFilterTypeDefault {
         return null;
     }
 
-    //<editor-fold desc="Commons Getter" defaultstate="collapsed">
+    @Override
     public String getClassName() {
         return this.getClass().getName();
     }
 
+    @Override
     public String getLabel() {
         return getName();
     }
 
+    @Override
     public String getPropertyOptions() {
         return AppUtil.readPluginResource(getClass().getName(), "/properties/DatetimeDataListFilter.json", null, true, "/messages/DatetimeDataListFilter");
     }
 
+    @Override
     public String getDescription() {
         return getClass().getPackage().getImplementationTitle();
     }
 
+    @Override
     public String getName() {
         return AppPluginUtil.getMessage("datalist.dtdlfp.title", getClassName(), "/messages/DatetimeDataListFilter");
     }
 
+    @Override
     public String getVersion() {
         return getClass().getPackage().getImplementationVersion();
     }
